91a11922877acd56c52a79ef71aa4a770c1c966c
Austin Elliott <74209030+a3lliott@users.noreply.github.com>
2020-11-20 15:17:34 -0600

    Added conditions check on showing/hiding buttons

--- src/Pagination.js
+++ src/Pagination.js
@@ -3,8 +3,9 @@ import React from 'react'
 export default function Pagination({ goToNextPage, goToPrevPage }) {
     return (
         <div>
-            <button onClick={goToPrevPage}>Previous</button>
-            <button onClick={goToNextPage}>Next</button>
+            {/* Tricky way to add condition on button */}
+            {goToPrevPage && <button onClick={goToPrevPage}>Previous</button>}
+            {goToNextPage && <button onClick={goToNextPage}>Next</button>}
         </div>
     )
 }
